65d1722576b245d6dfb357dd128c0c5806169887
andreicampigotto <andreicampigotto@gmail.com>
2018-06-08 16:22:46 -0300

    começando
    123

--- Museu/Museu/menuPrincipal.cs
+++ Museu/Museu/menuPrincipal.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Museu
+{
+    class menuPrincipal
+    {
+        Roupas roupas = new Roupas();
+        Armas armas = new Armas();
+        Quadros quadros = new Quadros();
+        Veiculos veiculos = new Veiculos();
+        Funcionarios funcionarios = new Funcionarios();
+        Fidelidade fidelidade = new Fidelidade();
+
+        public menuPrincipal()
+        {
+
+        }
+    }
+}
