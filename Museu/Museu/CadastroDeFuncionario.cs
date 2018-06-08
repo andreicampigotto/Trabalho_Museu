a4082d37a5e34a67cd413aa861c2afe7dcc8f4a1
Marcos Luiz Mittag Filho <marquinhoszx_xd@outlook.com>
2018-06-08 17:04:49 -0300

    ujkh

--- Museu/Museu/CadastroDeFuncionario.cs
+++ Museu/Museu/CadastroDeFuncionario.cs
@@ -8,5 +8,12 @@ namespace Museu
 {
     class CadastroDeFuncionario
     {
+        String[] nome = new String[30];
+        String[] sobrenome = new String[30];
+        int idade = 0;
+        String[] funcao = new String[30];
+
+        double salario = 0;
+        int horasSemanais = 0;
     }
 }
