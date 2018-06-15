dd221cf8e40a0144ef4de5839b39cef7051e078a
andreicampigotto <andreicampigotto@gmail.com>
2018-06-15 16:32:46 -0300

    bla
    bla

--- Museu/Museu/CadastroFidelidade.cs
+++ Museu/Museu/CadastroFidelidade.cs
@@ -86,7 +86,7 @@ namespace Museu
                             |-----------------------------------------|
                             |  2  |        LISTAR CLIENTES            |
                             |-----------------------------------------|
-                            |  3  |        ALTERAR CLIENTES           |
+                            |  3  |        EDITAR CLIENTES            |
                             |-----------------------------------------|
                             |  4  |      REMOVER CLIENTES             |
                             |-----------------------------------------|
@@ -146,6 +146,127 @@ namespace Museu
                 Console.WriteLine("\nNão foi possivel inserir!");
             }
         }
+
+        private void solicitarTelefoneContato(int n)
+        {
+            try
+            {
+                Console.Write("\nTelefone para Contato:");
+                telefoneContato[n] = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarTelefoneContato(n);
+            }
+        }
+
+        private void solicitarQuantidadeGastosNoMuseu(int n)
+        {
+            try
+            {
+                Console.Write("\nQuantidade gastos no museu");
+                quantidadeGastosNoMuseu[n] = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarQuantidadeGastosNoMuseu(n);
+            }
+        }
+
+        private void solicitarQuantidadeDeVisitas(int n)
+        {
+            try
+            {
+                Console.Write("\Quantidade de visitas:");
+                quantidadeDeVisitas[n] = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarQuantidadeDeVisitas(n);
+            }
+        }
+
+        private void solicitarSexo(int n)
+        {
+            Console.Write("\nSexo: ");
+            sexo[n] = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(sexo[n]) || sexo[n].Count() < 2)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarSexo(n);
+            }
+        }
+
+        private void solicitarProfissao(int n)
+        {
+            Console.Write("\nTurno: ");
+            profissao[n] = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(profissao[n]) || profissao[n].Count() < 2)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarProfissao(n);
+            }
+        }
+
+        private void solicitarNacionalidade(int n)
+        {
+            Console.Write("\nNacionalidade: ");
+            nacionalidade[n] = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(nacionalidade[n]) || nacionalidade[n].Count() < 2)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarNacionalidade(n);
+            }
+        }
+
+        private void solicitarIdade(int n)
+        {
+            try
+            {
+                Console.Write("\nIdade: ");
+                idade[n] = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarIdade(n);
+            }
+        }
+
+        private void solicitarSobrenome(int n)
+        {
+            Console.Write("\nSobrenome: ");
+            sobrenome[n] = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(sobrenome[n]) || sobrenome[n].Count() < 2)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarSobrenome(n);
+            }
+        }
+
+        private void solicitarNome(int n)
+        {
+            Console.Write("\nNome: ");
+            nome[n] = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(nome[n]) || nome[n].Count() < 2)
+            {
+                Console.WriteLine("\nDados inválidos!");
+                solicitarNome(n);
+            }
+        }
+
+        private void solicitarCpf(int n)
+        {
+            throw new NotImplementedException();
+        }
         public void listar()
         {
             Console.WriteLine("\n--- Lista de clientes ---");
@@ -157,13 +278,107 @@ namespace Museu
             Console.WriteLine("Média gastos por visita: R$" + imprimirMediaVisita());
             
         }
+        public void alterar()
+        {
+            Console.WriteLine("\n--- Editar dados do cliente ---");
+            inserirDados(buscarCliente());
+        }
+        public void remover()
+        {
+            int buscado = buscarCliente();
+            if (buscado >= 0)
+            {
+                for (int i = buscado; i < cont; i++)
+                {
+                    cod[i] = cod[i + 1];
+                    cpf[i] = cpf[i + 1];
+                    nome[i] = nome[i + 1];
+                    sobrenome[i] = sobrenome[i + 1];
+                    idade[i] = idade[i + 1];
+                    nacionalidade[i] = nacionalidade[i + 1];
+                    profissao[i] = profissao[i + 1];
+                    sexo[i] = sexo[i + 1];
+                    quantidadeDeVisitas[i] = quantidadeDeVisitas[i + 1];
+                    quantidadeGastosNoMuseu[i] = quantidadeGastosNoMuseu[i + 1];
+                    telefoneContato[i] = telefoneContato[i + 1];
+                }
+                Console.WriteLine("\nRemovido!");
+                cont--;
+            }
+            else
+            {
+                Console.WriteLine("\nNão foi possível excluir!");
+            }
+        }
 
+        //Buscas e Impressões
+        public void imprimirCliente(int i)
+        {
+            Console.WriteLine(string.Format("-Código:   {0}\nNome:    {1} Sobrenome: {2}\nIdade:   {3}\nNacionalidade:  {4}\nProfissão: {5} Sexo: {6}\nQuantidade de visitas: {7}\nQuantidade Gastos no museu: {8}\nTelefone para contato:  {9}"  cod[i],  nome[i], sobrenome[i], idade[i], nacionalidade[i], profissao[i], sexo[i], quantidadeDeVisitas[i], quantidadeGastosNoMuseu[i], telefoneContato[i]));
+            
+        }
+        public int buscarFuncionario()
+        {
+            int op = 0, buscado = -1;
 
+            Console.Write("\n--- Buscar cliente ---\n");
+            Console.Write("\n1- Buscar por Códiogo");
+            Console.Write("\n2- Buscar por nome");
+            Console.Write("\n3- Buscar por sobrenome");
+            Console.Write("\n0- Cancelar");
+            Console.Write("\nOpção: ");
+            op = Convert.ToInt32(Console.ReadLine());
 
+            if (op != 1 && op != 2 && op != 3) Console.WriteLine("\nSaindo...");
+            else buscado = buscarBy(op);
 
-                    
-        
+            return buscado;
+        }
 
+        public int buscarBy(int op)
+        {
+            int indice = -1, codF = 0;
+            String nomeF, sobrenomeF;
+
+            if (op == 2)
+            {
+                Console.Write("\nInforme o nome do cliente: ");
+                nomeF = Console.ReadLine();
+                for (int i = 0; i < cont; i++)
+                {
+                    if (nomeF.Equals(nome[i]))
+                    {
+                        imprimirCliente(i);
+                    }
+                }
+            }
+            else if (op == 3)
+            {
+                Console.Write("\nInforme o sobrenome do cliente: ");
+                sobrenomeF = Console.ReadLine();
+                for (int i = 0; i < cont; i++)
+                {
+                    if (sobrenomeF.Equals(sobrenome[i]))
+                    {
+                        imprimirCliente(i);
+                    }
+                }
+            }
+            Console.Write("\nInforme o código do cliente: ");
+            codF = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < cont; i++)
+            {
+                if (cod[i] == codF)
+                {
+                    indice = i;
+                }
+            }
+
+            return indice;
+        }
+
+       
         }
     }
 
