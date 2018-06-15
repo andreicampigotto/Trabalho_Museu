6344f8d237a2018bbaf128cca56274cbcb179b77
andreicampigotto <andreicampigotto@gmail.com>
2018-06-15 14:18:57 -0300

    bla
    bla

---
+++ Museu/Museu/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Museu
+{
+    class Utils
+    {   
+        public class Utils 
+    {
+        public static int PressKleyRetournZero(object value)
+        {
+            int valueReturn = 0; 
+
+            int.TryParse(value.ToString(),out valueReturn);
+
+            return valueReturn;
+        }
+       
+    }
+
+internal static int PressKleyRetournZero(string p)
+{
+ 	throw new NotImplementedException();
+}}
+    }
+}
